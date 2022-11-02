394b3f37674e13c98eda163191341f49eb5fa79c
Mov305 <abd3052001@gmail.com>
2022-11-02 20:07:25 +0200

    added all the req relations

--- src/person.rb
+++ src/person.rb
@@ -1,17 +1,23 @@
 # Person class
 require_relative 'nameable'
+require_relative 'rental'
 
 class Person < Nameable
-  def initialize(age, name = 'Unkonwn', permission: true)
+  def initialize(age, name = 'Unkonwn', rentals = [], permission: true)
     super()
     @id = Random.rand(1000)
     @name = name
     @age = age
     @parent_permission = permission
+    @rentals = rentals
   end
 
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+  end
 
   def age_of?
     @age >= 18
--- test.rb
+++ test.rb
@@ -1,6 +1,9 @@
 require './src/person'
 require './src/trimmer_name'
 require './src/capitalize_name'
+require './src/student'
+require './src/classroom'
+require './src/book'
 
 person = Person.new(22, 'maximilianus')
 person.correct_name
@@ -8,3 +11,11 @@ capitalized_person = Capitalize.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = Trimmer.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
+
+classroom = Classroom.new('Math')
+student = Student.new(22, classroom, 'maximilianus')
+puts student.classroom.label
+
+book = Book.new('The Lord of the Rings', 'J. R. R. Tolkien')
+book.add_rental('2021-01-01', person)
+puts book.rentals.inspect
