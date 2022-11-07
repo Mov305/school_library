5570041466a1d1dd6b40a39562c3b98e2acc0125
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-07 12:12:38 -0400

    Fix Linster and Add EoL rule for windows

--- src/options.rb
+++ src/options.rb
@@ -1,12 +1,12 @@
-require_relative "app"
+require_relative 'app'
 
 class Options
   $app = App.new
 
-	attr_reader :str_to_be_print
+  attr_reader :str_to_be_print
 
-	def initialize
-		@str_to_be_print = "WellCome to School Library App! \n
+  def initialize
+    @str_to_be_print = "WellCome to School Library App! \n
 		Please choose an option by entering a number: \n
 		1 - List all books \n
 		2 - List all people \n
@@ -15,52 +15,50 @@ class Options
 		5 - Create a rental \n
 		6 - List all rentals for a given person id \n
 		7 - Exit \n"
-	end
-
-
+  end
 
   def options(option)
     case option
     when 1
       $app.list_books
       sleep(0.5)
-      print "Press Enter to continue"
+      print 'Press Enter to continue'
       gets.chomp
       run
     when 2
       $app.list_people
       sleep(0.5)
-      print "Press Enter to continue"
+      print 'Press Enter to continue'
       gets.chomp
       run
     when 3
       $app.create_person
       sleep(0.5)
-      print "Press Enter to continue"
+      print 'Press Enter to continue'
       gets.chomp
       run
     when 4
       $app.create_book
       sleep(0.5)
-      print "Press Enter to continue"
+      print 'Press Enter to continue'
       gets.chomp
       run
     when 5
       $app.create_rental
       sleep(0.5)
-      print "Press Enter to continue"
+      print 'Press Enter to continue'
       gets.chomp
       run
     when 6
       $app.list_rentals_for_person_id
       sleep(0.5)
-      print "Press Enter to continue"
+      print 'Press Enter to continue'
       gets.chomp
       run
     when 7
-      puts "Thank you for using this app!"
+      puts 'Thank you for using this app!'
     else
-      puts "Invalid option"
+      puts 'Invalid option'
       run
     end
   end
