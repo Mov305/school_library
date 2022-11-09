c6b8d0c5a04a979a15fa9ce8118c3531da7c320d
Carlos Vivas <carlosmuerto@gmail.com>
2022-11-09 14:30:01 -0400

    Add classroom unit test and fix classroom

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require './src/person'
 require './src/rental'
 require './src/student'
 require './src/teacher'
+require './src/classroom'
 
 RSpec.configure do |config|
   config.formatter = :documentation
--- src/classroom.rb
+++ src/classroom.rb
@@ -5,10 +5,18 @@ class Classroom
     @students = students
   end
 
+	def remove_student(student)
+		@students.delete(student)
+	end
+
   def add_student(student)
     @students << student unless @students.include?(student)
+		if !student.classroom.nil? && student.classroom != self
+			student.classroom.remove_student student
+		end
+
     student.classroom = self
   end
 
-  attr_accessor :label
+  attr_reader :students, :label
 end
